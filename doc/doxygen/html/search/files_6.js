65dda1f307a4bb6aa51a4b7b063169b73c616f0e
SandraScherer <74064037+SandraScherer@users.noreply.github.com>
2022-10-14 23:09:44 +0200

    Update doxygen documentation

--- doc/doxygen/html/search/files_6.js
+++ doc/doxygen/html/search/files_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['genre_2ecs_0',['Genre.cs',['../d8/dbd/_genre_8cs.html',1,'']]],
-  ['genreitem_2ecs_1',['GenreItem.cs',['../d8/d7d/_genre_item_8cs.html',1,'']]]
+  ['genrecontentcreator_2ecs_1',['GenreContentCreator.cs',['../d6/df0/_genre_content_creator_8cs.html',1,'']]],
+  ['genreitem_2ecs_2',['GenreItem.cs',['../d8/d7d/_genre_item_8cs.html',1,'']]]
 ];
