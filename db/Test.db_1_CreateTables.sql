4edc3d883406b68c241e61fe3df0f1b4155195bd
SandraScherer <74064037+SandraScherer@users.noreply.github.com>
2023-01-23 22:14:54 +0100

    Update database creation queries with new table Series_Camera and test data

--- db/Test.db_1_CreateTables.sql
+++ db/Test.db_1_CreateTables.sql
@@ -1244,3 +1244,15 @@ CREATE TABLE Series_AspectRatio (
   FOREIGN KEY(SeriesID) REFERENCES Series(ID) ON UPDATE No action ON DELETE No action, 
   FOREIGN KEY(AspectRatioID) REFERENCES AspectRatio(ID) ON UPDATE No action ON DELETE No action, 
   FOREIGN KEY(StatusID) REFERENCES Status(ID) ON UPDATE No action ON DELETE No action);
+CREATE TABLE Series_Camera (
+  ID          text NOT NULL, 
+  SeriesID    text, 
+  CameraID    text, 
+  Details     text, 
+  Notes       text, 
+  StatusID    text, 
+  LastUpdated text, 
+  PRIMARY KEY (ID), 
+  FOREIGN KEY(SeriesID) REFERENCES Series(ID) ON UPDATE No action ON DELETE No action, 
+  FOREIGN KEY(CameraID) REFERENCES Camera(ID) ON UPDATE No action ON DELETE No action, 
+  FOREIGN KEY(StatusID) REFERENCES Status(ID) ON UPDATE No action ON DELETE No action);
