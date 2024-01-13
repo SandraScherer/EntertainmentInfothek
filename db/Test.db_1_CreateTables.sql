757dba16757708cf6b529d14ca1f97aeecc38016
SandraScherer <74064037+SandraScherer@users.noreply.github.com>
2024-01-13 19:46:56 +0100

    Update database creation queries with new table Series_ProductionDate and test data

--- db/Test.db_1_CreateTables.sql
+++ db/Test.db_1_CreateTables.sql
@@ -1327,3 +1327,15 @@ CREATE TABLE Series_FilmingDate (
   PRIMARY KEY (ID), 
   FOREIGN KEY(SeriesID) REFERENCES Series(ID) ON UPDATE No action ON DELETE No action, 
   FOREIGN KEY(StatusID) REFERENCES Status(ID) ON UPDATE No action ON DELETE No action);
+CREATE TABLE Series_ProductionDate (
+  ID          text NOT NULL, 
+  SeriesID     text, 
+  StartDate   text, 
+  EndDate     text, 
+  Details     text, 
+  Notes       text, 
+  StatusID    text, 
+  LastUpdated text, 
+  PRIMARY KEY (ID), 
+  FOREIGN KEY(SeriesID) REFERENCES Series(ID) ON UPDATE No action ON DELETE No action, 
+  FOREIGN KEY(StatusID) REFERENCES Status(ID) ON UPDATE No action ON DELETE No action);
