0b832a14a558e494256641812ca746a5a6fc290a
SandraScherer <74064037+SandraScherer@users.noreply.github.com>
2025-08-10 21:36:44 +0200

    Update NuGet package dependencies and target framework

--- _SonarQube_Analization_Start.cmd
+++ _SonarQube_Analization_Start.cmd
@@ -1,9 +1,9 @@
 dotnet tool install --global dotnet-sonarscanner
 dotnet tool install --global dotnet-coverage
 
-dotnet sonarscanner begin /k:"EntertainmentInfothek" /d:sonar.host.url="http://localhost:9000" /d:sonar.token="sqp_3acdc4ca1535f3b6bea382b279c48186d31134f6" /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml
+dotnet sonarscanner begin /k:"EntertainmentInfothek" /d:sonar.host.url="http://localhost:9000" /d:sonar.token="sqp_a450633dd4edefb1388f7e202b3f43308629a0c5" /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml
 dotnet build --no-incremental
 dotnet-coverage collect "dotnet test" -f xml -o "coverage.xml"
-dotnet sonarscanner end /d:sonar.token="sqp_3acdc4ca1535f3b6bea382b279c48186d31134f6"
+dotnet sonarscanner end /d:sonar.token="sqp_a450633dd4edefb1388f7e202b3f43308629a0c5"
 
 pause
