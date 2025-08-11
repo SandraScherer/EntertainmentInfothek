1a517450bee0eb65a01e3bc14fde3316bc9fc4dd
SandraScherer <74064037+SandraScherer@users.noreply.github.com>
2025-08-11 15:19:52 +0200

    Update database creation queries with new table Series_Director and test data

--- db/Test.db_1_CreateTables.sql
+++ db/Test.db_1_CreateTables.sql
@@ -1406,3 +1406,16 @@ CREATE TABLE Series_OtherCompany (
   FOREIGN KEY(SeriesID) REFERENCES Series(ID) ON UPDATE No action ON DELETE No action, 
   FOREIGN KEY(CompanyID) REFERENCES Company(ID) ON UPDATE No action ON DELETE No action, 
   FOREIGN KEY(StatusID) REFERENCES Status(ID) ON UPDATE No action ON DELETE No action);
+CREATE TABLE Series_Director (
+  ID          text NOT NULL, 
+  SeriesID    text, 
+  PersonID    text, 
+  Role        text, 
+  Details     text, 
+  Notes       text, 
+  StatusID    text, 
+  LastUpdated text, 
+  PRIMARY KEY (ID), 
+  FOREIGN KEY(SeriesID) REFERENCES Series(ID) ON UPDATE No action ON DELETE No action, 
+  FOREIGN KEY(PersonID) REFERENCES Person(ID) ON UPDATE No action ON DELETE No action, 
+  FOREIGN KEY(StatusID) REFERENCES Status(ID) ON UPDATE No action ON DELETE No action);
